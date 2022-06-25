b8fa7db03cb9576a3855c6e066ce572de1e41de5
Eldor Azimov <eldorazimov37@gmail.com>
2022-06-25 21:23:02 +0900

    Adding Validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -17,15 +17,34 @@ const AddUser = () => {
 
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredAge, enteredUsername);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="text" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="text"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
